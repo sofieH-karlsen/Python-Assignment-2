c0e37cff964218a87c75e2dbbfe0508e0c73e888
Sofie Karlsen <sofiehk@gmail.com>
2025-10-09 15:01:42 +0200

    changing setup and moving main Maybe finally i can run is as a package

--- setup.py
+++ setup.py
@@ -7,16 +7,15 @@ def read_requirements():
 setup(
     name="ACIT_StudyReminder",
     version="0.1",
-    packages=find_packages(),
-    py_modules=['main'],       # Includes main.py as a top-level module
+    packages=find_packages(),  # finds the acit_studyreminder package
     include_package_data=True,
-    description="A module as a part of ACIT4420 mandetory assignment 2, simulates the autimation of sending personalized study reminders",
+    description="A module for ACIT4420 assignment 2: automated study reminders",
     author="Sofie Karlsen",
     author_email="sokar8923@oslomet.no",
     install_requires=read_requirements(),
     entry_points={
         'console_scripts': [
-            'study_reminders=main:main',  # Points to main() in main.py (root directory)
+            'study_reminders=acit_studyreminder.main:main',  # points to main() inside main.py
         ],
     },
 )
