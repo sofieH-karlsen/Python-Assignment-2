cc8a19abf8aaac50d41e028bd39a47a26b0cceff
Sofie Karlsen <sofiehk@gmail.com>
2025-10-09 14:41:54 +0200

    Small fix in setup to mak sure requrments are read

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
 from setuptools import setup, find_packages
 
+def read_requirements():
+    with open("requirements.txt") as f:
+        return f.read().splitlines()
+
 setup(
     name="ACIT_StudyReminder",
     version="0.1",
@@ -9,7 +13,7 @@ setup(
     description="A module as a part of ACIT4420 mandetory assignment 2, simulates the autimation of sending personalized study reminders",
     author="Sofie Karlsen",
     author_email="sokar8923@oslomet.no",
-    install_requires=[requirements.txt],
+    install_requires=read_requirements(),
     entry_points={
         'console_scripts': [
             'study_reminders=main:main',  # Points to main() in main.py (root directory)
